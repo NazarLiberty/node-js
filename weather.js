bff6b86d6d7007062caebfa1121b6d6f54650eb7
noliynyk <n.oliynyk@seclgroup.com>
2023-09-29 21:10:52 +0300

    Lesson 39: Work with fs

--- weather.js
+++ weather.js
@@ -1,26 +1,35 @@
 #!/usr/bin/env node
-import { getArgs } from './helpers/args.js'
-import { printHelp } from './services/log.service.js'
-import { saveKeyValue } from './services/storage.service.js'
+import { getArgs } from "./helpers/args.js";
+import { printError, printHelp, printSuccess } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token saved successfully");
+  } catch (error) {
+    printError("Error while saving token: " + error)
+  }
+};
 
 const initCLI = () => {
-    const args = getArgs(process.argv)
+  const args = getArgs(process.argv);
 
-    if (args.h) {
-        // Output help
-        printHelp()
-    }
+  if (args.h) {
+    // Output help
+    printHelp();
+  }
 
-    if (args.s) {
-        // Save city
-    }
+  if (args.s) {
+    // Save city
+  }
 
-    if (args.t) {
-        // Save token
-        saveKeyValue("token", args.t)
-    }
+  if (args.t) {
+    // Save token
+    return saveToken(args.t)
+  }
 
-    // Output weather
-}
+  // Output weather
+};
 
 initCLI();
